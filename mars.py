f2984bfc73891d3dabd2e6295757bc97567b757d
dstarichkova <95042139+dstarichkova@users.noreply.github.com>
2022-02-17 12:08:25 +0300

    Результат отбора

--- mars.py
+++ mars.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -72,6 +72,125 @@ def promotion_image():
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>"""
+
+
+# @app.route('/astronaut_selection', methods=['POST', 'GET'])
+# def astronaut_selection():
+#     if request.method == 'GET':
+#         return f'''<!doctype html>
+#                         <html lang="en">
+#                           <head>
+#                             <meta charset="utf-8">
+#                             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+#                             <link rel="stylesheet"
+#                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+#                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+#                             crossorigin="anonymous">
+#                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+#                             <title>Пример формы</title>
+#                           </head>
+#                           <body>
+#                             <h1>Форма для регистрации в суперсекретной системе</h1>
+#                             <div>
+#                                 <form class="login_form" method="post">
+#                                     <input type="text" class="form-control" id="lastname" aria-describedby="lastnameHelp" placeholder="Введите фамилию" name="lastname">
+#                                     <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name">
+#                                     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+#                                     <input type="password" class="form-control" id="password" placeholder="Введите пароль" name="password">
+#                                     <div class="form-group">
+#                                         <label for="classSelect">Какое у Вас образование?</label>
+#                                         <select class="form-control" id="classSelect" name="class">
+#                                           <option>Начальное</option>
+#                                           <option>Среднее</option>
+#                                           <option>Высшее</option>
+#                                         </select>
+#                                      </div>
+#                                     </div>
+#                                     <div class="form-group">
+#                                         <label for="form-check">Какие у Вас есть профессии?</label>
+#                                         <div class="form-check">
+#                                           <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+#                                           <label class="form-check-label" for="male">
+#                                             Мужской
+#                                           </label>
+#                                         </div>
+#                                         <div class="form-check">
+#                                           <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+#                                           <label class="form-check-label" for="female">
+#                                             Женский
+#                                           </label>
+#                                         </div>
+#                                     </div>
+#                                     <div class="form-group">
+#                                         <label for="about">Немного о себе</label>
+#                                         <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+#                                     </div>
+#                                     <div class="form-group">
+#                                         <label for="photo">Приложите фотографию</label>
+#                                         <input type="file" class="form-control-file" id="photo" name="file">
+#                                     </div>
+#                                     <div class="form-group">
+#                                         <label for="form-check">Укажите пол</label>
+#                                         <div class="form-check">
+#                                           <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+#                                           <label class="form-check-label" for="male">
+#                                             Мужской
+#                                           </label>
+#                                         </div>
+#                                         <div class="form-check">
+#                                           <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+#                                           <label class="form-check-label" for="female">
+#                                             Женский
+#                                           </label>
+#                                         </div>
+#                                     </div>
+#                                     <div class="form-group form-check">
+#                                         <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+#                                         <label class="form-check-label" for="acceptRules">Готов быть добровольцем</label>
+#                                     </div>
+#                                     <button type="submit" class="btn btn-primary">Записаться</button>
+#                                 </form>
+#                             </div>
+#                           </body>
+#                         </html>'''
+#     elif request.method == 'POST':
+#         print(request.form['email'])
+#         print(request.form['password'])
+#         print(request.form['class'])
+#         print(request.form['file'])
+#         print(request.form['about'])
+#         print(request.form['accept'])
+#         print(request.form['sex'])
+#         return "Форма отправлена"
+
+
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <h3>Претендента на участие в миссии {nickname}:</h3>
+                    <div class="alert alert-success" role="alert">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+                    </div>
+                    <div class="alert alert-light" role="alert">
+                      Составляет {rating}!
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      Желаем удачи!
+                    </div>
+                  </body>
+                </html>'''
 
 
 if __name__ == "__main__":
